6d5a4ced063508478aa09e82d02af39184cf7cb4
Vincenz E <35447833+vincenz-e@users.noreply.github.com>
2022-05-03 17:10:53 +0200

    add data migration
    For migrating existing PaypalPayment objects with related_resource_state from update response

--- django_paypal/migrations/0004_paypalpayment_related_resource_state.py
+++ django_paypal/migrations/0004_paypalpayment_related_resource_state.py
@@ -3,6 +3,23 @@
 from django.db import migrations, models
 
 
+def migrate_resource_state(apps, schema_editor):
+    import json
+    PaypalPayment = apps.get_model('django_paypal', 'PaypalPayment')
+
+    for payment in PaypalPayment.objects.all():
+        if payment.update_response_object:
+            payment_response = json.loads(payment.update_response_object)
+            if payment_response['state'] == 'approved':
+                for transaction in payment_response['transactions']:
+                    if 'related_resources' in transaction:
+                        for resource in transaction['related_resources']:
+                            if 'sale' in resource:
+                                if 'state' in resource['sale']:
+                                    payment.related_resource_state = resource['sale']['state']
+                                    payment.save(update_fields=['related_resource_state'])
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,4 +32,5 @@ class Migration(migrations.Migration):
             name='related_resource_state',
             field=models.CharField(blank=True, max_length=255, null=True, verbose_name='related resource state'),
         ),
+        migrations.RunPython(migrate_resource_state, reverse_code=migrations.RunPython.noop, elidable=True)
     ]
